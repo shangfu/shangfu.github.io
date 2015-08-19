072eac3552c86a00e38a2f7fd7516fc0acdeea4c
shangfu <pengshangfu@gmail.com>
2015-08-19 21:31:34 +0000

    Added About page and first post!

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 rake generate      # 生成页面
 #rake preview       # 如果需要在本地预览生成的结果，预览页http://localhost:4000
+cp CNAME _deploy/CNAME
 rake deploy        #发布到github
 # 发布到github的pages在master分支，还要保存源的source分支
 git add .
